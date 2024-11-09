b150ced283428fbb7571155d063035bbb2a79357
Irfan Hussain <I.hussain@expressmedia.int>
2024-11-09 10:23:44 +0500

    resole error 1

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+// import localFont from "next/font/local";
 import "./globals.css";
 // import Contact from "./contact/page";
 // import About from "./about/page";
